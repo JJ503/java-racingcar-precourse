edd609996b02989767a4b560d33bb350ac9e7306
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-02 16:36:21 +0900

    feat(InputView): 입력 값이 숫자가 아니라면 예외 발생 기능 추가
    docs: 입력 값이 숫자가 아니라면 예외 발생 완료 체크

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,9 +4,20 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
 
     public String readCarNames() {
         System.out.println(INPUT_CAR_NAMES);
         return Console.readLine();
+    }
+
+    public int readTryCount() {
+        System.out.println(INPUT_TRY_COUNT);
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException numberFormatException) {
+            ExceptionMessage.INPUT_WRONG_TRY_COUNT_MESSAGE.throwException();
+            return readTryCount();
+        }
     }
 }
