73dd2764061786bef025da22fdc6efc9810670a9
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-15 23:03:48 +0900

    style(InputView): 횟수 입력 메시지 수정
    횟수 입력 메시지가 잘못 선언되어 있었기에 수정한다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
     private static final String INPUT_RACING_CAR_NAMES
             = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_RACING_TRY_COUNT
-            = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+            = "시도할 회수는 몇회인가요?";
 
     public List<String> readRacingCarNames() {
         System.out.println(INPUT_RACING_CAR_NAMES);
