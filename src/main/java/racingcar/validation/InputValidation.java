1c6721dc870fcaed0005952385d97fe7efe0c645
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-15 21:21:22 +0900

    style(InputValidation): 함수를 static으로 변경

--- src/main/java/racingcar/validation/InputValidation.java
+++ src/main/java/racingcar/validation/InputValidation.java
@@ -8,30 +8,30 @@ import java.util.List;
 public class InputValidation {
     private static final String COMMA = ",";
 
-    public List<String> validateCarNames(String names) {
+    public static List<String> validateCarNames(String names) {
         isExist(names);
         List<String> carNames = toList(names);
         areAllNamesMoreThanFive(carNames);
         return carNames;
     }
 
-    private void isExist(String checkValue) {
+    private static void isExist(String checkValue) {
         if (checkValue.isEmpty()) {
             ExceptionMessage.NONE_INPUT.throwException();
         }
     }
 
-    private List<String> toList(String names) {
+    private static List<String> toList(String names) {
         return Arrays.asList(names.split(COMMA));
     }
 
-    private void areAllNamesMoreThanFive(List<String> carNames) {
+    private static void areAllNamesMoreThanFive(List<String> carNames) {
         for (String name : carNames) {
             isMoreThanFive(name.length());
         }
     }
 
-    private void isMoreThanFive(int checkSize) {
+    private static void isMoreThanFive(int checkSize) {
         if (checkSize > 5) {
             ExceptionMessage.INPUT_NAME_MORE_THAN_FIVE.throwException();
         }
