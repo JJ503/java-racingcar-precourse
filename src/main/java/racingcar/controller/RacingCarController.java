5cbac09be2ff75dc0d3b9ed2d567aefd56a2ad02
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-15 23:06:35 +0900

    feat(RacingCarController): 객체 초기화
    docs: 객체 초기화 완료 체크

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,9 @@
+package racingcar.controller;
+
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingCarController {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+}
