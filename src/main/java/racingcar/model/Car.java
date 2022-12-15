021328f5a7f8a2c10280980c8e95d0bc27925612
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-15 22:58:26 +0900

    feat(Car): 자동차 이름, 위치 정보 가져오기 기능 추가
    docs: 자동차 이름, 위치 정보 가져오기 완료 체크

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,6 +1,10 @@
 package racingcar.model;
 
 import racingcar.constants.MovementState;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -19,5 +23,9 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    public List<Object> getCarInformation() {
+        return Arrays.asList(name, position);
     }
 }
