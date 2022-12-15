7b62e57302da80280d92cdd9378c3120e4b9dc75
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-15 23:07:26 +0900

    feat(RacingCarController): 경주할 자동차 이름 입력 받기 및 저장 기능 추가
    docs: 경주할 자동차 이름 입력 받기 및 저장 완료 체크

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,9 +1,31 @@
 package racingcar.controller;
 
+import racingcar.model.Car;
+import racingcar.model.Cars;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
+
+import java.util.List;
 
 public class RacingCarController {
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
+
+    public void startRacingGame() {
+        Cars cars = initRacingCarNames();
+    }
+
+    private Cars initRacingCarNames() {
+        List<String> carNames = inputView.readRacingCarNames();
+        return initCars(carNames);
+    }
+
+    private Cars initCars(List<String> carNames) {
+        Cars cars = new Cars();
+        for (String name : carNames) {
+            cars.addCar(new Car(name));
+        }
+
+        return cars;
+    }
 }
