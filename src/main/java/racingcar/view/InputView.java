c7f1c232a2f4c3554860179b65caced386eb88f2
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-15 21:21:54 +0900

    feat(InputView): 자동차 이름 입력 검증 기능 연결
    docs: 경주할 자동차 이름 입력 값 검증하기 완료 체크

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,12 +1,15 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.validation.InputValidation;
+
+import java.util.List;
 
 public class InputView {
     private static final String INPUT_RACING_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
-    public String readRacingCarNames() {
+    public List<String> readRacingCarNames() {
         System.out.println(INPUT_RACING_CAR_NAMES);
-        return Console.readLine();
+        return InputValidation.validateCarNames(Console.readLine());
     }
 }
