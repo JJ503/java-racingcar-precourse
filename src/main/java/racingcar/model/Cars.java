36442f1a5de45e5a211f5866eec86fa69d50ab2e
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-15 23:01:53 +0900

    feat(Cars): 각 자동차별로 전진 혹은 멈춤에 대한 무작위 값 받아 반영하기 기능 추가
    docs: 각 자동차별로 전진 혹은 멈춤에 대한 무작위 값 받기 및 결과 반연하기 완료 체크

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,12 +1,21 @@
 package racingcar.model;
 
+import racingcar.util.GoOrStopValueGenerator;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
+    private static final int ONE = 1;
     private final List<Car> cars = new ArrayList<>();
 
     public void addCar(Car car) {
         cars.add(car);
+    }
+
+    public void tryRacingOnce() {
+        for (Car car : cars) {
+            car.addPosition(new GoOrStopValueGenerator().generate());
+        }
     }
 }
