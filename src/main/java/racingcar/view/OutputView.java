5b96585ce59dcd6b83c8f4a24fe0e8ca81d3a391
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-15 23:10:12 +0900

    feat(OutputView): 게임 시작 메시지 출력 기능 추가
    docs: 게임 시작 메시지 출력 완료 체크

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+import java.util.List;
+
+public class OutputView {
+    private static final String START_GAME_MESSAGE = "\n실행 결과";
+
+    public void printStartGameMessage() {
+        System.out.println(START_GAME_MESSAGE);
+    }
+
+}
