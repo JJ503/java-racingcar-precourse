89f2d8b2ccb14ec9519eec0c0880bb70b387cc0f
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-15 23:44:56 +0900

    test(InputValidation): 입력 값 검증 테스트 추가
    1. 자동차 이름 입력 결과 테스트
    - 정상 데이터 입력
    - 비정상 데이터 입력
    
    2. 시도 횟수 입력 결과 테스트
    - 정상 데이터 입력
    - 비정상 데이터 입력

---
+++ src/test/java/racingcar/validation/InputValidationTest.java
@@ -0,0 +1,46 @@
+package racingcar.validation;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class InputValidationTest {
+
+    @DisplayName("자동차 이름 입력 결과 테스트 - 정상 데이터 입력")
+    @ValueSource(strings = {"pobi,woni,jun", "pobi, woni, jun", "pobi,woni, jun", "pobi,woni,jun,"})
+    @ParameterizedTest
+    void validateNormalCarNames(String name) {
+        List<String> expectResult = Arrays.asList("pobi", "woni", "jun");
+        assertThat(InputValidation.validateCarNames(name)).isEqualTo(expectResult);
+    }
+
+    @DisplayName("자동차 이름 입력 결과 테스트 - 비정상 데이터 입력")
+    @ValueSource(strings = {"pobi,woni,junnnnnnn", "", "     "})
+    @ParameterizedTest
+    void validateExceptionCarNames(String name) {
+        assertThatThrownBy(() -> InputValidation.validateCarNames(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("시도 횟수 입력 결과 테스트 - 정상 데이터 입력")
+    @CsvSource(value = {"5:5", "10:10", "15:15", "20:20"}, delimiter = ':')
+    @ParameterizedTest
+    void validateNormalTryCount(String tryCount, int expectResult) {
+        assertThat(InputValidation.validateTryCount(tryCount)).isEqualTo(expectResult);
+    }
+
+    @DisplayName("시도 횟수 입력 결과 테스트 - 비정상 데이터 입력")
+    @ValueSource(strings = {"", "     ", "2147483648", "sdf123", "sdfsdf"})
+    @ParameterizedTest
+    void validateExceptionTryCount(String tryCount) {
+        assertThatThrownBy(() -> InputValidation.validateTryCount(tryCount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
