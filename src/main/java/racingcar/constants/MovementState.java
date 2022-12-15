99e3cbdc0a5045fa8e82f816f0903d7c1cd918b9
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-15 21:42:45 +0900

    feat(MovementState): 값에 따라 전진 상태 결정 및 전달 기능 추가
    docs: 값에 따라 전진 상태 결정 및 전달 및 관련 기능 완료 체크

--- src/main/java/racingcar/constants/MovementState.java
+++ src/main/java/racingcar/constants/MovementState.java
@@ -4,9 +4,18 @@ public enum MovementState {
     GO(1),
     STOP(0);
 
+    private static int MINIMUM_FOR_MOVEMENT = 4;
     private final int movement;
 
     MovementState(int movement) {
         this.movement = movement;
+    }
+
+    public static MovementState getMovementState(int number) {
+        if (MINIMUM_FOR_MOVEMENT <= number) {
+            return GO;
+        }
+
+        return STOP;
     }
 }
